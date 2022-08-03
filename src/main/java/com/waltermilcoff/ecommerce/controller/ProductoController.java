4a1133ecea370288d654465b78f8984614edfd90
waltermilcoff <waltermilcoff@gmail.com>
2022-08-03 18:45:29 -0300

    Creando metodo guardar Producto y agregando LOGGER al Controller Producto

--- src/main/java/com/waltermilcoff/ecommerce/controller/ProductoController.java
+++ src/main/java/com/waltermilcoff/ecommerce/controller/ProductoController.java
@@ -1,12 +1,19 @@
 package com.waltermilcoff.ecommerce.controller;
 
+import com.waltermilcoff.ecommerce.model.Producto;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.logging.Logger;
 
 @Controller
 @RequestMapping("/productos")
 public class ProductoController {
+
+    private final Logger LOGGER = (Logger) LoggerFactory.getLogger(ProductoController.class);
 
 
     @GetMapping("")
@@ -18,5 +25,11 @@ public class ProductoController {
     @GetMapping("/create")
     public String create(){
         return "productos/create";
+    }
+
+    @PostMapping("/save")
+    public String save(Producto producto){
+        LOGGER.info("Este es el objeto producto {}", producto);
+        return "redirect:/productos";
     }
 }
